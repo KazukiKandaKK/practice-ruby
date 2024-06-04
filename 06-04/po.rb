1a171f5c25847428064f8951740802774012c9f5
KazukiKanda <kazuki.kanda05570@gmal.com>
2024-06-04 18:19:52 +0900

    逆ポーランド記法の計算をやろうとしている

---
+++ 06-04/po.rb
@@ -0,0 +1,24 @@
+def calc(s_calc)
+    num_list = []
+    op_list = []
+    s_calc.each_char {
+        |c| if ['+', '*', '-', '/', '(', ')'].include?(c) then 
+                op_list.push(c)
+            else
+                num_list.push(c)
+            end
+    }
+    num_tmp1 = 0
+    num_tmp2 = 0
+    op_tmp1 = 0
+    op_tmp2 = 0
+    while num_list.length!=0 && op_list.length!=0 
+        num_tmp1 = num_list.pop.to_i
+        num_tmp2 = num_list.pop.to_i
+        op_tmp = op_list.pop
+        num_list.push(eval("#{num_tmp1}#{op_tmp}#{num_tmp2}"))
+    end
+    p num_list
+end
+
+calc("1+2+3+4")

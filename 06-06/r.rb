9abf13b2902a7802079e5b0973efcb6cb383347b
KazukiKanda <kazuki.kanda05570@gmal.com>
2024-06-06 18:58:52 +0900

    ネスト深いのをとりあえず

--- 06-06/r.rb
+++ 06-06/r.rb
@@ -8,19 +8,19 @@ def calc(s_calc)
             # contain the operand
             op_list.push(c)
             is_pre_num = false
-        else
-            # contain number.
-            if num_list.length>=1 && op_list.length==1 then
-                num_list.push(eval("#{num_list.pop.to_i}#{op_list.pop}#{c.to_i}"))
-            # if input the two-digit.
-            elsif is_pre_num then
-                num_list.push("#{num_list.pop}#{c.to_i}".to_i) 
-            # other case push stack.
-            else
-                num_list.push(c.to_i)
-            end
-            is_pre_num = true
+            next
         end
+        # contain number.
+        if num_list.length>=1 && op_list.length==1 then
+            num_list.push(eval("#{num_list.pop.to_i}#{op_list.pop}#{c.to_i}"))
+        # if input the two-digit.
+        elsif is_pre_num then
+            num_list.push("#{num_list.pop}#{c.to_i}".to_i) 
+        # other case push stack.
+        else
+            num_list.push(c.to_i)
+        end
+        is_pre_num = true
     }
     return num_list[0]
 end

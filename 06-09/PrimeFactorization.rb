f57337db877746c96ae084ced3627703e0daac9a
KazukiKanda <kazuki.kanda05570@gmal.com>
2024-06-09 19:32:41 +0900

    多分素因数分解できる

---
+++ 06-09/PrimeFactorization.rb
@@ -0,0 +1,31 @@
+$rets = []
+
+def calc(target_num)
+    ret = 0
+    1.upto(target_num) do |num|
+        if num==0 then
+            next
+        end
+        if target_num%num == 0 && num != 1 && target_num >= num then
+            ret = target_num/num
+            $rets.push(num)
+            break
+        end
+    end
+    # p ret
+    if ret > 1 then
+        calc(ret)
+    end
+end
+
+# とりあえず1000までやる。
+target_num = ARGV[0].to_i
+
+# check args
+if target_num <= 1 || target_num > 1000 then
+    p "Err"
+end
+
+calc(target_num)
+
+p $rets

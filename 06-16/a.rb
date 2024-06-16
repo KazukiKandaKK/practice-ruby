b6a94657e6f1ba86d2a7c384afbbdc245a0ce6a5
KazukiKanda <kazuki.kanda05570@gmal.com>
2024-06-16 21:07:37 +0900

    mudanajouken

--- 06-16/a.rb
+++ 06-16/a.rb
@@ -1,17 +1,19 @@
 # @param {Integer} n
 # @return {Boolean}
 def is_happy(n)
-    if n>1 && n<=4 then
-        return false
-    end
+    judge(n)
     ans = 0
     n.to_s.each_char do |c|
         ans += c.to_i**2
     end
-    if ans.to_s.length <= 1 && ans==1 then
-         return true
-    elsif ans.to_s.length <= 1 && ans<=3 then
+    judge(ans)
+    is_happy(ans)
+end
+
+def judge(n)
+    if n==1 then
+        return true    
+    elsif n>1 && n<=4 then
         return false
     end
-    is_happy(ans)
 end
